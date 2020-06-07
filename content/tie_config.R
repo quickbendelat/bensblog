c6910e1c2afaf3ca31bf4c7df76a05b1c386f60c
Ben Gin <gin.benjamin@gmail.com>
2020-06-07 13:42:43 +1200

    tie calculation * create tables for all multipliers * edit function to use the multiplier tables

--- content/tie_config.R
+++ content/tie_config.R
@@ -1,8 +1,7 @@
 # change the date when updating the table 
 tbl_update_date <- "2020-06-02"
 
-# tie summary table
-
+# tie summary tablej for inputting new ties
 ties_input_df <- ties_data <- tibble::tribble(~name,              ~no_ties, ~no_steps,  ~fold_in_half, ~end_tied_with, ~url,                                              ~shaping_difficulty,       ~no_loop_thrus,
                                               "double_eldredge",  2,        6,          "N",           "little", "https://www.youtube.com/watch?v=jLYWJX4779A",     3,                         3,  
                                               "double_trinity",   1,        6,          "N",           "little", "https://www.youtube.com/watch?v=6-0FdqYjv6s",     1,                         6,
@@ -21,7 +20,53 @@ ties_input_df <- ties_data <- tibble::tribble(~name,              ~no_ties, ~no_
                                               "embrace",          1,        3,          "Y",           "little", "https://www.youtube.com/watch?v=mwfLpoyqeKE",     1,                         3,
                                               "deivao_twist",     1,        6,          "N",           "little", "https://www.youtube.com/watch?v=mCANkZKtES4",     1,                         3)  
 
+# tie column definitions
+base_tie_rating_tbl <- tibble::tribble(~shaping_diff, ~base,
+                                        1,             1,
+                                        2,             1.5,
+                                        3,             2,
+                                        4,             2.8,
+                                        5,             3.2)
 
+no_steps_mult_tbl <- tibble::tribble(~no_steps, ~mult,
+                                     1,         1,
+                                     2,         1,
+                                     3,         1,
+                                     4,         1.3,
+                                     5,         1.3,
+                                     6,         1.3,
+                                     7,         1.5,
+                                     8,         1.5,
+                                     9,         1.5,
+                                     10,        1.5)
+
+no_loops_mult_tbl <- tibble::tribble(~no_loops,  ~mult,
+                                     0,          1,
+                                     1,          1.1,
+                                     2,          1.2,
+                                     3,          1.3,
+                                     4,          1.5,
+                                     5,          1.6,
+                                     6,          1.7,
+                                     7,          1.8,
+                                     8,          1.9,
+                                     9,          2.0,
+                                     10,         2.1)
+
+no_ties_mult_tbl <- tibble::tribble(~no_ties, ~mult,
+                                    1,         1,
+                                    2,         1.2)
+
+fold_in_half_mult_tbl <- tibble::tribble(~fold_in_half, ~mult,
+                                         "N",         1,
+                                         "Y",         1.3)
+
+end_tied_mult_tbl <- tibble::tribble(~end_tied, ~mult,
+                                     "little",  1,
+                                     "big",     1.5,
+                                     "both",    1.5)
+
+# tie summary data table configured for the main page
 ties_data <- ties_input_df  %>% 
   mutate(img_id = row_number(),
          close_id = img_id - 1,
@@ -53,4 +98,13 @@ span.onclick = function() {
   unnest(overall_difficulty) %>% 
   mutate(overall_difficulty = round(10 * overall_difficulty/max(overall_difficulty), digits = 2)) %>%
   select(name, overall_difficulty, image)
-  # select(name, overall_difficulty, no_ties, no_steps, shaping_difficulty, no_loop_thrus, image)
+
+# detailed time summary data table
+ties_detailed_tbl <- ties_input_df  %>% 
+  mutate(image = paste0('<img src = "/img/', name, '.jpg"></img>'),
+         name = paste0("<a href='", url, "'target='_blank'>", name, "</a>")
+  ) %>% 
+  mutate(overall_difficulty = pmap(list(shaping_difficulty, no_ties, no_steps, fold_in_half, no_loop_thrus, end_tied_with), tie_overalldiff_calc)) %>% 
+  unnest(overall_difficulty) %>% 
+  mutate(overall_difficulty = round(10 * overall_difficulty/max(overall_difficulty), digits = 2)) %>%
+  select(name, overall_difficulty, shaping_difficulty, no_ties, no_steps, no_loop_thrus, image)
--- content/tie_functions.R
+++ content/tie_functions.R
@@ -1,48 +1,34 @@
 tie_overalldiff_calc <- function(shaping_diff, no_ties, no_steps, fold_in_half, no_loops, end_tied) {
   
-  no_ties_mult <- ifelse(no_ties == 1, 1, 1.2) 
+  no_ties_mult <- no_ties %>% 
+    enframe(name = NULL, value = "no_ties") %>% 
+    left_join(no_ties_mult_tbl, by = "no_ties") %>% 
+    pull(mult)
   
-  fold_in_half_mult <- ifelse(fold_in_half == "N", 1, 1.3) 
+  fold_in_half_mult <- fold_in_half %>% 
+    enframe(name = NULL, value = "fold_in_half") %>% 
+    left_join(fold_in_half_mult_tbl, by = "fold_in_half") %>% 
+    pull(mult)
   
-  end_tied_mult <- ifelse(end_tied == "both", 1.5, 1) 
+  end_tied_mult <- end_tied %>% 
+    enframe(name = NULL, value = "end_tied") %>% 
+    left_join(end_tied_mult_tbl, by = "end_tied") %>% 
+    pull(mult)
   
-  if(no_steps < 4) {
-    no_steps_mult <- 1
-  } else if(no_steps < 7) {
-    no_steps_mult <- 1.3
-  } else {
-    no_steps_mult <- 1.5
-  }
-  
-  no_loops_mult_tbl <- tibble::tribble(~no_loops,  ~mult,
-                                       0,          1,
-                                       1,          1.1,
-                                       2,          1.2,
-                                       3,          1.3,
-                                       4,          1.5,
-                                       5,          1.6,
-                                       6,          1.7,
-                                       7,          1.8,
-                                       8,          1.9,
-                                       9,          2.0,
-                                       10,         2.1)
+  no_steps_mult <- no_steps %>% 
+    enframe(name = NULL, value = "no_steps") %>% 
+    left_join(no_steps_mult_tbl, by = "no_steps") %>% 
+    pull(mult)
   
   no_loops_mult <- no_loops %>% 
     enframe(name = NULL, value = "no_loops") %>% 
     left_join(no_loops_mult_tbl, by = "no_loops") %>% 
     pull(mult)
   
-  if(shaping_diff == 1) {
-    base <- 1
-  } else if(shaping_diff == 2) {
-    base <- 1.5
-  } else if(shaping_diff == 3) {
-    base <- 2
-  } else if(shaping_diff == 4) {
-    base <- 2.8
-  } else {
-    base <- 3.2
-  }
+  base <- shaping_diff %>% 
+    enframe(name = NULL, value = "shaping_diff") %>% 
+    left_join(base_tie_rating_tbl, by = "shaping_diff") %>% 
+    pull(base)
   
   overall_diff <- base * no_ties_mult * fold_in_half_mult * end_tied_mult  * no_steps_mult  * no_loops_mult
   
