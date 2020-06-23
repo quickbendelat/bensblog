0d04d5685db1084b6722d8336b4f9b4b275f526e
Ben Gin <gin.benjamin@gmail.com>
2020-06-23 22:31:05 +1200

    ties update * add more tie images and data for summary tables * update base difficulty table

--- content/tie_config.R
+++ content/tie_config.R
@@ -1,25 +1,32 @@
 # change the date when updating the table 
-tbl_update_date <- "2020-06-09"
+tbl_update_date <- "2020-06-23"
 
 # tie summary table for inputting new ties
-ties_input_df <- ties_data <- tibble::tribble(~name,              ~no_ties, ~no_steps,  ~fold_in_half, ~end_tied_with, ~url,                                              ~shaping_difficulty,       ~no_loop_thrus,
-                                              "intrinity",        1,        4,          "Y",           "little",       "https://www.youtube.com/watch?v=38uEMP_jEkg",     2,                         4,
-                                              "double_eldredge",  2,        6,          "N",           "little",       "https://www.youtube.com/watch?v=jLYWJX4779A",     3,                         3,  
-                                              "double_trinity",   1,        6,          "N",           "little",       "https://www.youtube.com/watch?v=6-0FdqYjv6s",     1,                         6,
-                                              "eldredge",         1,        6,          "N",           "little",       "https://www.youtube.com/watch?v=FXyWSAmwVZ4",     1,                         0,
-                                              "eldredge2",        1,        6,          "N",           "little",       "https://www.youtube.com/watch?v=FukqiewJuPw",     2,                         2,
-                                              "trinity_eldredge", 1,        5,          "N",           "little",       "https://www.youtube.com/watch?v=Ls_MgWrTVrk",     1,                         2,
-                                              "trinity",          1,        4,          "N",           "little",       "https://www.youtube.com/watch?v=IX_GixAhXeY",     2,                         2,
-                                              "rosebud1",         1,        6,          "Y",           "little",       "https://www.youtube.com/watch?v=s-S1WSFTpuU",     2,                         1,
-                                              "rosebud2",         1,        4,          "N",           "little",       "https://www.youtube.com/watch?v=-diYX2FH_AE",     2,                         0,
-                                              "vidalia",          1,        5,          "N",           "little",       "https://www.youtube.com/watch?v=ENYCWzrB9d8",     2,                         3,
-                                              "valentine",        1,        5,          "Y",           "little",       "https://www.youtube.com/watch?v=XwvNQYIZnSI",     3,                         1,
-                                              "sweetheart",       1,        4,          "Y",           "little",       "https://www.youtube.com/watch?v=nfoqEexLghI",     4,                         3,
-                                              "romance",          1,        5,          "Y",           "little",       "https://www.youtube.com/watch?v=XdRsQ3KuTqM",     3,                         5,
-                                              "cupid",            1,        3,          "N",           "both",         "https://www.youtube.com/watch?v=VWfEccMG_HI",     5,                         1,
-                                              "vampire",          1,        5,          "Y",           "little",       "https://www.youtube.com/watch?v=wFTkThHwagI",     1,                         0,
-                                              "embrace",          1,        3,          "Y",           "little",       "https://www.youtube.com/watch?v=mwfLpoyqeKE",     2,                         3,
-                                              "deivao_twist",     1,        6,          "N",           "little",       "https://www.youtube.com/watch?v=mCANkZKtES4",     1,                         3)  
+ties_input_df <- tibble::tribble(
+                                 ~name,                        ~no_ties, ~no_steps, ~no_loop_thrus, ~shaping_difficulty, ~fold_in_half, ~end_tied_with,  ~url,
+                                 "pentanity",                  1,        6,         3,              2,                   "N",           "little",         NA,
+                                 "caped_eldredge",             1,        7,         4,              8,                   "Y",           "little",        "https://www.youtube.com/watch?v=6X3DdiKq-jc",
+                                 "double_trinity_eldredge",    2,        7,         7,              1,                   "N",           "little",         NA,
+                                 "double_intrinity",           2,        5,         4,              2,                   "Y",           "little",        "https://www.youtube.com/watch?v=G11Be_KRWr8",
+                                 "water",                      1,        4,         7,              3,                   "Y",           "little",         NA,
+                                 "intrinity",                  1,        4,         4,              2,                   "Y",           "little",        "https://www.youtube.com/watch?v=38uEMP_jEkg",
+                                 "double_eldredge",            2,        6,         3,              3,                   "N",           "little",        "https://www.youtube.com/watch?v=jLYWJX4779A",
+                                 "double_trinity",             1,        6,         6,              1,                   "N",           "little",        "https://www.youtube.com/watch?v=6-0FdqYjv6s",
+                                 "eldredge",                   1,        6,         0,              1,                   "N",           "little",        "https://www.youtube.com/watch?v=FXyWSAmwVZ4",
+                                 "eldredge2",                  1,        6,         2,              2,                   "N",           "little",        "https://www.youtube.com/watch?v=FukqiewJuPw",
+                                 "trinity_eldredge",           1,        5,         2,              1,                   "N",           "little",        "https://www.youtube.com/watch?v=Ls_MgWrTVrk",
+                                 "trinity",                    1,        4,         2,              2,                   "N",           "little",        "https://www.youtube.com/watch?v=IX_GixAhXeY",
+                                 "rosebud1",                   1,        6,         1,              2,                   "Y",           "little",        "https://www.youtube.com/watch?v=s-S1WSFTpuU",
+                                 "rosebud2",                   1,        4,         0,              2,                   "N",           "little",        "https://www.youtube.com/watch?v=-diYX2FH_AE",
+                                 "vidalia",                    1,        5,         3,              2,                   "N",           "little",        "https://www.youtube.com/watch?v=ENYCWzrB9d8",
+                                 "valentine",                  1,        5,         1,              3,                   "Y",           "little",        "https://www.youtube.com/watch?v=XwvNQYIZnSI",
+                                 "sweetheart",                 1,        4,         3,              4,                   "Y",           "little",        "https://www.youtube.com/watch?v=nfoqEexLghI",
+                                 "romance",                    1,        5,         5,              3,                   "Y",           "little",        "https://www.youtube.com/watch?v=XdRsQ3KuTqM",
+                                 "cupid",                      1,        3,         1,              5,                   "N",           "both",          "https://www.youtube.com/watch?v=VWfEccMG_HI",
+                                 "vampire",                    1,        5,         0,              1,                   "Y",           "little",        "https://www.youtube.com/watch?v=wFTkThHwagI",
+                                 "embrace",                    1,        3,         3,              2,                   "Y",           "little",        "https://www.youtube.com/watch?v=mwfLpoyqeKE",
+                                 "deivao_twist",               1,        6,         3,              1,                   "N",           "little",        "https://www.youtube.com/watch?v=mCANkZKtES4"
+                   )
 
 # tie column definitions
 base_tie_rating_tbl <- tibble::tribble(~shaping_diff, ~base,
@@ -27,7 +34,10 @@ base_tie_rating_tbl <- tibble::tribble(~shaping_diff, ~base,
                                         2,             1.5,
                                         3,             2,
                                         4,             2.8,
-                                        5,             3.2)
+                                        5,             3.2,
+                                        6,             3.6,
+                                        7,             4,
+                                        8,             4.4)
 
 no_steps_mult_tbl <- tibble::tribble(~no_steps, ~mult,
                                      1,         1,
@@ -92,8 +102,9 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 </script>'),
-         name = paste0("<a href='", url, "'target='_blank'>", name, "</a>")
-  ) %>% 
+         name = case_when(!is.na(url) ~ paste0("<a href='", url, "'target='_blank'>", name, "</a>"),
+                          TRUE ~ name)
+         ) %>% 
   select(-url, -img_id, -close_id) %>% 
   mutate(overall_difficulty = pmap(list(shaping_difficulty, no_ties, no_steps, fold_in_half, no_loop_thrus, end_tied_with), tie_overalldiff_calc)) %>% 
   unnest(overall_difficulty) %>% 
@@ -103,7 +114,8 @@ span.onclick = function() {
 # detailed time summary data table
 ties_detailed_tbl <- ties_input_df  %>% 
   mutate(image = paste0('<img src = "/img/', name, '.jpg"></img>'),
-         name = paste0("<a href='", url, "'target='_blank'>", name, "</a>")
+         name = case_when(!is.na(url) ~ paste0("<a href='", url, "'target='_blank'>", name, "</a>"),
+                          TRUE ~ name)
   ) %>% 
   mutate(overall_difficulty = pmap(list(shaping_difficulty, no_ties, no_steps, fold_in_half, no_loop_thrus, end_tied_with), tie_overalldiff_calc)) %>% 
   unnest(overall_difficulty) %>% 
