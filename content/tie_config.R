37a62e4ea65b0da6550b26057a374a2d4f45adfd
Ben Gin <gin.benjamin@gmail.com>
2020-08-11 20:15:15 +1200

    cosmo tie * add new tie

--- content/tie_config.R
+++ content/tie_config.R
@@ -1,9 +1,10 @@
 # change the date when updating the table 
-tbl_update_date <- "2020-07-28"
+tbl_update_date <- "2020-08-11"
 
 # tie summary table for inputting new ties
 ties_input_df <- tibble::tribble(
                                  ~name,                        ~no_ties, ~no_steps, ~no_loop_thrus, ~shaping_difficulty, ~fold_in_half, ~end_tied_with,  ~url,
+                                 "cosmo",                      1,        6,         1,              1,                   "N",           "little",        "https://www.youtube.com/watch?v=whxZ9j3rTgk",
                                  "nebula",                     1,        5,         0,              2,                   "N",           "little",         NA,
                                  "eclipse",                    1,        4,         1,              2,                   "Y",           "little",        "https://www.youtube.com/watch?v=IhbSTCJLJzU",
                                  "shuttle",                    1,        6,         3,              5,                   "Y",           "little",        "https://www.youtube.com/watch?v=Cb7wP8rWvnI",
