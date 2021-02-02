71f7c1c37a0d33807e8cec94e27ae9b6e0418164
Ben Gin <gin.benjamin@gmail.com>
2021-02-02 23:17:05 +1300

    edison tie knot * add tie config for edison knot

--- content/tie_config.R
+++ content/tie_config.R
@@ -1,9 +1,10 @@
 # change the date when updating the table 
-tbl_update_date <- "2020-12-16"
+tbl_update_date <- "2021-02-02"
 
 # tie summary table for inputting new ties
 ties_input_df <- tibble::tribble(
                                  ~name,                        ~no_ties, ~no_steps, ~no_loop_thrus, ~shaping_difficulty, ~fold_in_half, ~end_tied_with,  ~url,
+                                 "edison",                     1,        5,         2,              1,                   "Y",           "little",        "https://www.youtube.com/watch?v=mq9r0IGQ3SQ",
                                  "lily",                       1,        5,         1,              1,                   "Y",           "little",         NA,
                                  "fibonacci",                  1,        6,         4,              1,                   "Y",           "little",        "https://www.youtube.com/watch?v=JiNksSWvm2g",
                                  "fusion",                     1,        4,         2,              1,                   "N",           "little",        "https://www.youtube.com/watch?v=aefkk3Hxl4c",
